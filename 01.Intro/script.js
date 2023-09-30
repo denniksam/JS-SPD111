1ff28666eda8b44c7595beca5cd1f068afd45c7f
Denys Samoilenko <denniksam@gmail.com>
2023-09-30 17:48:10 +0300

    Intro

---
+++ 01.Intro/script.js
@@ -0,0 +1,7 @@
+console.log("Hello from file")  // ; не необхідна
+console.log("Hello 2 from file")  // розрив рядка - теж розділення команд
+
+function pressMeClick() {
+    res = prompt('How old are you?');
+    console.log(res + 1);
+}
